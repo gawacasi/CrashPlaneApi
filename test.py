cfc30d0fbc43408581bb6a1aeaa51fd8c26d64b7
gawacasi <gwcs@cin.ufpe.br>
2025-04-22 18:47:11 -0300

    [ Test.py ]

--- test.py
+++ test.py
@@ -3,10 +3,9 @@ import pandas as pd
 # Carregar os dados
 df = pd.read_csv("Airplane_Crashes_and_Fatalities_Since_1908.csv")
 
-# Obter todos os valores únicos da coluna 'Location'
-locations = df['Location'].unique()
-print("Localizações únicas:", locations)
+def get_unique_locations(df):
+    return sorted(df['Location'].unique())
 
-# Obter todos os valores únicos da coluna 'Operator'
-operators = df['Operator'].unique()
-print("Operadores únicos:", operators)
+def get_unique_operators(df):
+    return sorted(df['Operator'].unique())
+
